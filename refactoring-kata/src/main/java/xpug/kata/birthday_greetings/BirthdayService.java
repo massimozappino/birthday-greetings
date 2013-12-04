6e9f0f78e5f311c9720c0ae46b1aa537eca05fbf
Massimo Zappino <massimo.zappino@gmail.com>
2013-12-04 12:13:10 +0100

    Delegare la logica di recupero employees ad un oggetto EmployeeRepository

--- refactoring-kata/src/main/java/xpug/kata/birthday_greetings/BirthdayService.java
+++ refactoring-kata/src/main/java/xpug/kata/birthday_greetings/BirthdayService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.LinkedList;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -16,19 +17,19 @@ import javax.mail.internet.MimeMessage;
 public class BirthdayService {
 
 	public void sendGreetings(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
-		BufferedReader in = new BufferedReader(new FileReader(fileName));
-		String str = "";
-		str = in.readLine(); // skip header
-		while ((str = in.readLine()) != null) {
-			String[] employeeData = str.split(", ");
-			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
-			if (employee.isBirthday(xDate)) {
-				String recipient = employee.getEmail();
-				String body = "Happy Birthday, dear %NAME%".replace("%NAME%", employee.getFirstName());
-				String subject = "Happy Birthday!";
-				sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
-			}
-		}
+
+;
+        for (Employee employee : new EmployeeRepository().getEmployees(fileName)) {
+            if (employee.isBirthday(xDate)) {
+                String recipient = employee.getEmail();
+                String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
+                String subject = "Happy Birthday!";
+                sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
+            }
+        }
+
+
+
 	}
 
 	private void sendMessage(String smtpHost, int smtpPort, String sender, String subject, String body, String recipient) throws AddressException, MessagingException {
